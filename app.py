ae6c4987ffc42afb6c2005af52e060d03ced2348
benny <benjamin.schaeling@platform3l.com>
2024-09-26 14:16:08 +0200

    new webapp

--- app.py
+++ app.py
@@ -11,19 +11,19 @@ print(cwd)
 @app.route('/')
 def index():
     round_id = procs.get_monitor_round()
-    last_song = procs.get_last_song()
+    last_song = procs.get_last_question()
     buzzer_blocked = procs.get_buzzer_blocked()
     countdown = procs.countdown()
     if round_id == 0:
         return render_template('start.html', countdown=countdown)
     if round_id == 9:
-        return render_template('pause.html', last_song=last_song)
+        return render_template('pause.html')
     if round_id == 8:
         player_name = procs.get_player_name(1,8)
         return render_template('end.html', player_name=player_name)
     else:
         points = procs.show_points(round_id)
-        return render_template(f"index{buzzer_blocked}.html", points=points, last_song=last_song)
+        return render_template(f"index{buzzer_blocked}.html", points=points, last_question=last_question)
 
 if __name__ == "__main__":
     app.run()
