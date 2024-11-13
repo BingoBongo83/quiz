fdfcb786ce43f09e7212e5f25a696cfcb5f19830
benny <benjamin.schaeling@platform3l.com>
2024-11-13 16:16:33 +0100

    changed song to question overall

--- buzzer.py
+++ buzzer.py
@@ -6,21 +6,17 @@ while True:
     buzzer_pressed = procs.get_buzzer_pressed_from_serial()
     if buzzer_pressed == "0":
         print("BUZZER PLAYER 1 PRESSED")
-        procs.media_pause()
         procs.play_buzzer()
         procs.set_buzzer_pressed("0")
     elif buzzer_pressed == "1":
         print("BUZZER PLAYER 2 PRESSED")
-        procs.media_pause()
         procs.play_buzzer()
         procs.set_buzzer_pressed("1")
     elif buzzer_pressed == "2":
         print("BUZZER PLAYER 3 PRESSED")
-        procs.media_pause()
         procs.play_buzzer()
         procs.set_buzzer_pressed("2")
     elif buzzer_pressed == "3":
         print("BUZZER PLAYER 4 PRESSED")
-        procs.media_pause()
         procs.play_buzzer()
         procs.set_buzzer_pressed("3")
