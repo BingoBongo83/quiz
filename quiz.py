3bb52e27768d154f12990a0731c3357614d41699
benny <benjamin.schaeling@platform3l.com>
2024-11-14 13:43:11 +0100

    fixed maximum questions

--- quiz.py
+++ quiz.py
@@ -2571,9 +2571,10 @@ class GameWindow(QMainWindow):
         previous_question = {}
         previous_question = procs.get_last_question()
         maximum_questions = procs.get_round_maximum_questions(my_round_id)
-        print(f"maximum: {next_question.get("total", maximum_questions)}")
-        if maximum_questions > int({next_question.get('total', maximum_questions)}):
-            maximum_questions = next_question.get('total', maximum_questions)
+        # print(f"maximum: {next_question.get("total", maximum_questions)}")
+        maximum_questions_db = next_question.get("total", maximum_questions)
+        if maximum_questions > int(maximum_questions_db):
+            maximum_questions = maximum_questions_db
         questionsToPlay = maximum_questions - next_question.get("seq", 0) + 1
         question_count = (
             f"question# {next_question.get('seq',0)} / {maximum_questions} (noch {questionsToPlay}) "
