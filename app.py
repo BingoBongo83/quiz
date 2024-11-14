292b4f30bb7f8c747569323a36469667a02af5f6
benny <benjamin.schaeling@platform3l.com>
2024-11-14 10:28:00 +0100

    fixed editor

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ print(cwd)
 def index():
     round_id = procs.get_play_round()
     monitor_round = procs.get_monitor_round()
-    last_song = procs.get_last_song()
+    last_question = procs.get_last_question()
     buzzer_blocked = procs.get_buzzer_blocked()
     if int(buzzer_blocked) == 5:
         buzzer_blocked = 4
@@ -22,16 +22,16 @@ def index():
     if monitor_round == 0:
         return render_template('start.html', countdown=countdown)
     if monitor_round == 9:
-        return render_template('pause.html', last_song=last_song)
+        return render_template('pause.html', last_question=last_question)
     if monitor_round == 8:
         player_name = procs.get_player_name(1,8)
         return render_template('end.html', player_name=player_name)
     if monitor_round == 11:
-        return render_template('cover.html', last_song=last_song, cover=last_song['cover'], round=round_id)
+        return render_template('image.html', last_question=last_question, image=last_question['image'], round=round_id)
         print("cover")
     else:
         points = procs.show_points(round_id)
-        return render_template(f"index{buzzer_blocked}.html", points=points, last_song=last_song)
+        return render_template(f"index{buzzer_blocked}.html", points=points, last_question=last_question)
     procs.set_monitor_round_by_round_id(round_id)
     # print(f"set monitor to round {round_id}")
 
