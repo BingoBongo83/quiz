a7de7c372de59b9073c5757e65e9b883f380febf
benny <benjamin.schaeling@platform3l.com>
2024-11-18 08:11:06 +0100

    add maxQuestions

--- quiz.py
+++ quiz.py
@@ -27,7 +27,7 @@ from PySide6.QtCharts import (
     QChartView,
     QValueAxis,
 )
-from PySide6.QtCore import Qt, QSize, QRect, QTimer, 
+from PySide6.QtCore import Qt, QSize, QRect, QTimer
 from PySide6.QtGui import QFont, QPixmap, QAction, QIcon, QPainter, QBrush, QColor, QIntValidator
 from handlers import procs
 from config import (
